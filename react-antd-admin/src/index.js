98b4edeca13e9706f3c54746cb04b4477f5e88b2
a26053200 <zhengnan262@qq.com>
2018-10-30 01:16:16 +0800

    提交react+antd+react-route-dom架构

--- react-antd-admin/src/index.js
+++ react-antd-admin/src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom' // 路由
+import { LocaleProvider } from 'antd'
+import zh_CN from 'antd/lib/locale-provider/zh_CN'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <LocaleProvider locale={zh_CN}>
+            <App/>
+        </LocaleProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
